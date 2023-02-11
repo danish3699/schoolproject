115c52c11c1f8ad27c1b8267e2b3e4be97689db9
danish3699 <dbutt5854@gmail.com>
2023-02-11 21:30:14 +0500

    test

--- dani.js
+++ dani.js
@@ -1,5 +1,6 @@
-const user= Number(prompt("enter your number"))
 
-for(table=1;table<=10;table++){
-  document.write(`${user} * ${table} = ${table*user} <br>`)
+const value = [3,5,8];
+for (x=0;x<=2;x++){
+ for(b=1;b<=10;b++)
+  console.log(`${value[x]} X ${[b]} = ${value[x]* [b]} `)
 }
