777b169c9fa7132b56136d9d3460a20ff1c4c4ab
danish3699 <dbutt5854@gmail.com>
2023-02-20 09:01:38 +0500

    test

---
+++ dani1.js
@@ -0,0 +1,41 @@
+
+// const value1 = 20;
+// const value2 = 20;
+// if (value1>value2) {
+//     console.log("this value is greater : "+value1)
+// } else if (value1<value2) {
+//     console.log("this value is greater : "+value2)
+// }else if (value1===value2) {
+//     console.log("both values are equal")
+// }  else {
+//  console.log("please enter integer value")   
+// }
+
+
+
+
+
+
+for(x=1;x<=15;x++){
+    if (x % 2==0) {
+        console.log("even number: "+x)
+     } else if(x % 2 !=0) {
+        console.log("odd number : "+x)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
